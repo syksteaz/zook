bbeb87bb24376dfec45c489e2357dcecd9365067
edouard winia <edouardwinia@gmail.com>
2015-11-01 12:19:41 +0100

    routes added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :cocktails, only: [:index, :show, :new, :create] do
+    resources :doses, only: [:new, :create, :destroy]
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
