fac9449052d33b5c7b2f4737cde7a693373aa317
edouard winia <edouardwinia@gmail.com>
2015-11-01 19:44:36 +0100

    action create cocktail done

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,10 +8,16 @@
 
 
 cocktail1 = Cocktail.new(name: "ginfizz")
+cocktail1.save
 cocktail2 = Cocktail.new(name: "whisky sour")
+cocktail2.save
 cocktail3 = Cocktail.new(name: "mojito")
+cocktail3.save
 
 ingredient1 = Ingredient.new(name: "mint")
+ingredient1.save
 ingredient2 = Ingredient.new(name: "sugar")
+ingredient2.save
 ingredient3 = Ingredient.new(name: "lemon")
+ingredient3.save
 
